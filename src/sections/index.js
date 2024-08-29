a2a70e033177ffae1596e9b02fb1be7cf4b9a402
komal2203 <capkomal2203@gmail.com>
2024-08-29 14:03:57 +0530

    deploy project to github

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+import PopularProducts from "./PopularProducts";
+import Footer from "./Footer";
+
+export {
+  Hero,
+  CustomerReviews,
+  SuperQuality,
+  SpecialOffer,
+  Services,
+  Footer,
+  PopularProducts,
+  Subscribe,
+};
